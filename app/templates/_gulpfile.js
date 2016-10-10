4e3755591d97165d50fba7f9cbcf9ef87931c3ee
Evan Thibodeau <evan.thibodeau@liferay.com>
2016-10-10 18:32:40 +0200

    Convert lexicon soy.

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -7,7 +7,8 @@ metal.registerTasks({
 	bundleFileName: '<%= kebabCaseName %>.js',<% if (buildFormat !== 'globals') { %>
 	mainBuildJsTasks: ['build:<%= buildFormat %>:js'],<% } %>
 	moduleName: '<%= repoName %>'<% if (templateLanguage !== 'Soy') { %>,
-	noSoy: true<% } %><% if (isNodeModule) { %>,
+	noSoy: true<% } %><% if (templateLanguage === 'Soy') { %>,
+	soyDeps: ['node_modules/*lexicon*/**/*.soy', 'node_modules/*metal*/**/*.soy']<% } %><% if (isNodeModule) { %>,
 	testNodeSrc: [
 		'env/test/node.js',
 		'test/**/*.js'
